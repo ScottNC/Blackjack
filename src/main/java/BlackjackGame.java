259c30ca0e431570cc822baa5edc3820822de58c
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-11-07 17:13:42 +0000

    find score where aces can be 1 or 11

--- src/main/java/BlackjackGame.java
+++ src/main/java/BlackjackGame.java
@@ -9,10 +9,19 @@ public class BlackjackGame {
         System.out.println("Welcome to Blackjack!");
         String[] cards = DeckManager.drawCards(deckId, 2);
         System.out.println(Arrays.toString(cards));
-        System.out.println("Total score: " + sumCards(cards));
+        System.out.println("Total score: " + Arrays.toString(findScore(cards)));
     }
 
-    public static int sumCards(String[] cards) {
+    public static int[] findScore(String[] cards) {
+        int sumCards = findSumCards(cards);
+
+        if (sumCards <= 21 && containsAce(cards))
+            return new int[]{sumCards, sumCards + 10};
+
+        return new int[]{sumCards};
+    }
+
+    public static int findSumCards(String[] cards) {
         return Arrays.stream(cards)
                 .map(card -> card.charAt(0))
                 .map(BlackjackGame::findValue)
@@ -26,5 +35,9 @@ public class BlackjackGame {
             case '0', 'Q', 'J', 'K' -> 10;
             default -> parseInt(String.valueOf(c));
         };
+    }
+
+    public static boolean containsAce(String[] cards) {
+        return String.join("", cards).contains("A");
     }
 }
--- src/test/java/BlackjackGameTest.java
+++ src/test/java/BlackjackGameTest.java
@@ -12,7 +12,15 @@ class BlackjackGameTest {
 
     @Test
     void sumCards() {
-        assertEquals(11, BlackjackGame.sumCards(new String[]{"5H", "6S"}));
-        assertEquals(25, BlackjackGame.sumCards(new String[]{"KD", "QC", "5H"}));
+        assertEquals(11, BlackjackGame.findSumCards(new String[]{"5H", "6S"}));
+        assertEquals(25, BlackjackGame.findSumCards(new String[]{"KD", "QC", "5H"}));
+    }
+
+    @Test
+    void findScore() {
+        assertArrayEquals(new int[]{11}, BlackjackGame.findScore(new String[]{"5H", "6S"}));
+        assertArrayEquals(new int[]{25}, BlackjackGame.findScore(new String[]{"KD", "QC", "5H"}));
+        assertArrayEquals(new int[]{26}, BlackjackGame.findScore(new String[]{"KD", "QC", "5H", "AS"}));
+        assertArrayEquals(new int[]{8,18}, BlackjackGame.findScore(new String[]{"AH", "7H"}));
     }
 }
